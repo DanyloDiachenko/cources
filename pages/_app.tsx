617d95b3b2ec5e79e42325591107885663b252b1
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-18 12:41:53 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Cources - лучшие курсы в 2023</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
