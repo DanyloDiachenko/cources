cb951d5c0f474c7f974d768b80b11260723c1c59
Danylo Diachenko <danildiachenko23@gmail.com>
2022-12-18 23:46:16 +0200

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Htag } from '../components';
 import { Button } from '../components';
+import { P } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +8,9 @@ export default function Home(): JSX.Element {
       <Htag tag="h3">Текст</Htag>
       <Button appearance="primary" arrow="right">Click me</Button>
       <Button appearance="ghost" arrow="down">Click me</Button>
+      <P size="s">abc</P>
+      <P size="m">abc</P>
+      <P size="l">abc</P>
     </>
   );
 }
